0ae31cc7348e210acfb416fd3610a5b695a25bbc
Johanna <109289350+JohannaSah@users.noreply.github.com>
2023-09-19 16:18:14 +0200

    feat add const data containing all typography information

---
+++ src/routes/typography/+page.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	const data = [
+		{
+			header: {
+				stylename: 'Display 2xl',
+				styledata: 'Font size: 72px / 4.5rem | Line height: 90px / 5.625rem | Tracking: -2%'
+			},
+			tailwind: 'untld-display-2xl'
+		},
+		{
+			header: {
+				stylename: 'Display xl',
+				styledata: 'Font size: 60px / 3.75rem | Line height: 72px / 4.5rem | Tracking: -2%'
+			},
+			tailwind: 'untld-display-xl'
+		},
+		{
+			header: {
+				stylename: 'Display lg',
+				styledata: 'Font size: 48px / 3rem | Line height: 60px / 3.75rem | Tracking: -2%'
+			},
+			tailwind: 'untld-display-lg'
+		},
+		{
+			header: {
+				stylename: 'Display md',
+				styledata: 'Font size: 36px / 2.25rem | Line height: 44px / 2.75rem | Tracking: -2%'
+			},
+			tailwind: 'untld-display-md'
+		},
+		{
+			header: {
+				stylename: 'Display sm',
+				styledata: 'Font size: 30px / 1.875rem | Line height: 38px / 2.375rem'
+			},
+			tailwind: 'untld-display-sm'
+		},
+		{
+			header: {
+				stylename: 'Text xl',
+				styledata: 'Font size: 20px / 1.25rem | Line height: 30px / 1.875rem'
+			},
+			tailwind: 'untld-text-xl'
+		},
+		{
+			header: {
+				stylename: 'Text lg',
+				styledata: 'Font size: 18px / 1.125rem | Line height: 28px / 1.75rem'
+			},
+			tailwind: 'untld-text-lg'
+		},
+		{
+			header: {
+				stylename: 'Text md',
+				styledata: 'Font size: 16px / 1rem | Line height: 24px / 1.5rem'
+			},
+			tailwind: 'untld-text-md'
+		},
+		{
+			header: {
+				stylename: 'Text sm',
+				styledata: 'Font size: 14px / 0.875rem | Line height: 20px / 1.25rem'
+			},
+			tailwind: 'untld-text-sm'
+		},
+		{
+			header: {
+				stylename: 'Text xs',
+				styledata: 'Font size: 12px / 0.75rem | Line height: 18px / 1.125rem'
+			},
+			tailwind: 'untld-text-xs'
+		}
+	];
+</script>
