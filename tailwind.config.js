8177560ea81a7afba657aafe602a6a39966a61c2
Nadiem <nadiem8@msn.com>
2023-09-20 10:13:09 +0200

    Update tailwind.config.js to contain the safeList of all colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 import untitledPlugin from './src/lib/tailwindPlugin';
+import { colors } from './src/lib/tailwindPlugin';
 
 /** @type {import('tailwindcss').Config} */
+
+const generateClassNames = colors => {
+    return Object.values(colors).flatMap(mainValue => {
+        return Object.entries(mainValue).flatMap(([colorKey, colorValue]) => {
+            const formattedColorKey = colorKey.replace(/\s+/g, '-').toLowerCase();
+                return Object.keys(colorValue).map(shade => `bg-untld-${formattedColorKey}-${shade}`);
+        });
+    });
+};
+
+const allClassNames = generateClassNames(colors);
+
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
 			colors: {
-				'untitled-primary': {
-					50: '#fdf8f6',
-					100: '#f2e8e5',
-					200: '#43302b'
-				}
+			
 			}
 		}
 	},
-	plugins: [untitledPlugin]
+	// For now we need this safelist, we won't need this in every project described in our docs. We only need this for the docs page
+	safelist:[...allClassNames],
+	plugins: [untitledPlugin],
 };
