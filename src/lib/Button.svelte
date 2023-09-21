7380c201e54df9fea122ec804e0891542f05e235
Johanna <109289350+JohannaSah@users.noreply.github.com>
2023-09-21 16:09:23 +0200

    work in progress

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -12,77 +12,137 @@
 		| { type: 'dot' }
 		| { type: 'icon-only'; leading: string }
 		| { type: 'icon'; leading?: string; trailing?: string };
-	type state = 'default' | 'hover' | 'focused' | 'disabled';
 
 	export let size: size;
 	export let hierarchy: hierarchy = 'primary';
 	export let destructive: boolean = false;
 	export let icon: icon;
-	export let state: state = 'default';
+
+    /* TODO: What colors for shadow and focused shadow?*/
+    const styles = {
+        destructive: {
+            primary: {
+                coloring: 'bg-untld-primary-600 hover:bg-untld-primary-700 text-untld-base-white border-untld-primary-600 shadow-{color} focused:shadow-{color} disabled:bg-untld-primary-200 '
+            }
+            'secondary-gray': { 
+                coloring: 'bg-untld-base-white text-untld-gray-700 border-untld-gray-300 shadow-{color} hover:bg-untld-gray-50 hover:text-untld-gray-800 focused:shadow-{color} disabled:bg-untld-base-white focused:shadow-{color} disabled:text-untld-gray-300'
+            }
+            'secondary-color': {
+                coloring: 'bg-{color} text-{color} border-{color} shadow-{color} hover:bg-{color}'
+            }
+            'tertiary-gray': {
+                coloring: 'bg-{color} text-{color} border-{color} shadow-{color} hover:bg-{color}'
+            }
+            'tertiary-color': {
+                coloring: 'bg-{color} text-{color} border-{color} shadow-{color} hover:bg-{color}'
+            }
+            'link-gray': {
+                coloring: 'bg-{color} text-{color} border-{color} shadow-{color} hover:bg-{color}'
+            }
+            'link-color': {
+                coloring: 'bg-{color} text-{color} border-{color} shadow-{color} hover:bg-{color}'
+            }
+        } 
+        notDestructive: {
+            primary: {
+                coloring: 'bg-{color} text-{color} border-{color} shadow-{color} hover:bg-{color}'
+            }
+            'secondary-gray': {
+                coloring: 'bg-{color} text-{color} border-{color} shadow-{color} hover:bg-{color}'
+            }
+            'secondary-color': {
+                coloring: 'bg-{color} text-{color} border-{color} shadow-{color} hover:bg-{color}'
+            }
+            'tertiary-gray': {
+                coloring: 'bg-{color} text-{color} border-{color} shadow-{color} hover:bg-{color}'
+            }
+            'tertiary-color': {
+                coloring: 'bg-{color} text-{color} border-{color} shadow-{color} hover:bg-{color}'
+            }
+            'link-gray': {
+                coloring: 'bg-{color} text-{color} border-{color} shadow-{color} hover:bg-{color}'
+            }
+            'link-color': {
+                coloring: 'bg-{color} text-{color} border-{color} shadow-{color} hover:bg-{color}'
+            }
+        }
+    }
 </script>
 
 <!--  TODO add correct: colors,   -->
 <button
 	class="
-        {`h-${
-            size === 'sm'
-            ? '36'
-            : size === 'md'
-            ? '40'
-            : size === 'lg'
-            ? '44'
-            : size === 'xl'
-            ? '48'
-            : size === '2xl'
-            ? '60'
-            : ''
-        }`}
-        {{	hierarchy === 'primary' && destructive === 'false'          // where do I define the colors used? which colors do I use? Do I use color variables?
-            ? 'bg-{color} text-{color} border-{color} shadow-{color}'
-            : hierarchy === 'secondary-gray' && destructive === false
-            ? 'bg-{color} text-{color} border-{color} shadow-{color}'
-            : hierarchy === 'secondary-color' && destructive === false
-            ? 'bg-{color} text-{color} border-{color} shadow-{color}'
-            : hierarchy === 'tertiary-gray' && destructive === false
-            ? 'bg-{color} text-{color} border-{color} shadow-{color}'
-            : hierarchy === 'tertiary-color' && destructive === false
-            ? 'bg-{color} text-{color} border-{color} shadow-{color}'
-            : hierarchy === 'link-gray' && destructive === false
-            ? 'bg-{color} text-{color} border-{color} shadow-{color}'
-            : hierarchy === 'link-color' && destructive === false
-            ? 'bg-{color} text-{color} border-{color} shadow-{color}'
-            : hierarchy === 'primary' && destructive === true
-            ? 'bg-{color} text-{color} border-{color} shadow-{color}'
-            : hierarchy === 'secondary-gray' && destructive === true
-            ? 'bg-{color} text-{color} border-{color} shadow-{color}'
-            : hierarchy === 'secondary-color' && destructive === true
-            ? 'bg-{color} text-{color} border-{color} shadow-{color}'
-            : hierarchy === 'tertiary-gray' && destructive === true
-            ? 'bg-{color} text-{color} border-{color} shadow-{color}'
-            : hierarchy === 'tertiary-color' && destructive === true
-            ? 'bg-{color} text-{color} border-{color} shadow-{color}'
-            : hierarchy === 'link-gray' && destructive === true
-            ? 'bg-{color} text-{color} border-{color} shadow-{color}'
-            : hierarchy === 'link-color' && destructive === true
-            ? 'bg-{color} text-{color} border-{color} shadow-{color}'
-            : hierarchy === 'primary' && destructive === false
-        }}
-        border border-radius-[0.5rem] border-solid shadow-sm hover:shadow-lg
+        {size === 'sm'
+		? 'h-36'
+		: size === 'md'
+		? 'h-40'
+		: size === 'lg'
+		? 'h-44'
+		: size === 'xl'
+		? 'h-48'
+		: size === '2xl'
+		? 'h-60'
+		: ''}
+        {!destructive
+		? hierarchy === 'primary'
+			? ''
+			: hierarchy === 'secondary-gray'
+			? ''
+			: hierarchy === 'secondary-color'
+			? ''
+			: hierarchy === 'tertiary-gray'
+			? ''
+			: hierarchy === 'tertiary-color'
+			? ''
+			: hierarchy === 'link-gray'
+			? '' // link-color below
+			: ''
+		: hierarchy === 'primary' && destructive === true
+		? ''
+		: hierarchy === 'secondary-gray' && destructive === true
+		? ''
+		: hierarchy === 'secondary-color' && destructive === true
+		? ''
+		: hierarchy === 'tertiary-gray' && destructive === true
+		? ''
+		: hierarchy === 'tertiary-color' && destructive === true
+		? ''
+		: hierarchy === 'link-gray' && destructive === true
+		? '' // link-color below
+		: 'bg-{color} hover:bg-{color} text-{color} border-{color} shadow-{color}'} 
+        border border-radius-[0.5rem] border-solid shadow-sm focus:shadow-lg
     "
 >
-    <div 
-        class="
-        {{ icon === 'icon' && (leading || trailing) ? 'urls' :
-           icon === 'icon-only' && leading === '' ? 'url' :
-           icon === 'dot' ? '' : 'icon' }}
-    "
-    >
-        <p 
-            class="
-                {{ icon === 'icon' ? '' : icon === 'icon-only' ? 'hidden' : icon === 'dot' ? '' : icon === 'icon'}}
+	<!-- TODO finish the component structure and the classes for the structure -->
+	<div>
+		<div
+			class="
+            {icon.type === 'icon'
+				? ''
+				: icon.type === 'icon-only'
+				? '' // dot below
+				: ''}
+        "
+		/>
+		<p
+			class="
+                {icon.type === 'icon'
+				? ''
+				: icon.type === 'icon-only'
+				? 'hidden' // dot below
+				: ''} 
             "
-        >
-            example text
-        </p>
-    </div>
+		>
+			example text
+		</p>
+		<div
+			class="
+            {icon.type === 'icon'
+				? ''
+				: icon.type === 'icon-only'
+				? '' // dot below
+				: ''}
+        "
+		/>
+	</div>
 </button>
