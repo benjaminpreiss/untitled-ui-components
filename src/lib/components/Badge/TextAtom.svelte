5b481b96febcd514b4dc6a8626616c30b3b4511c
Nadiem <nadiem8@msn.com>
2023-10-04 08:58:24 +0200

    Add TextAtom.svelte

---
+++ src/lib/components/Badge/TextAtom.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+	import type { size } from './types.js';
+	import type { HTMLAttributes } from 'svelte/elements';
+	const size = getContext<size>('size');
+
+	const styleSizes = {
+		sm: {
+			text: 'untld-text-xs'
+		},
+		md: {
+			text: 'untld-text-sm'
+		},
+		lg: {
+			text: 'untld-text-md leading-5 '
+		}
+	};
+</script>
+
+<span {...$$props} class="{$$props.class}  untld-text-medium {styleSizes[size].text}">
+	<slot />
+</span>
