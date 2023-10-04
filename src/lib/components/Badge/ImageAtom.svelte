aa9d27452bd3a2d96fb780f48cd4c94d7f92dd23
Nadiem <nadiem8@msn.com>
2023-10-04 08:58:46 +0200

    Add ImageAtom.svelte, temporary solution

---
+++ src/lib/components/Badge/ImageAtom.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+	interface $$Props extends HTMLAttributes<HTMLSpanElement> {
+		url: string;
+	}
+	export let url: string;
+</script>
+
+<!-- todo temporary solution, how can we make sure that images passed in are also rendered? Just like icons without the ImageAtom -->
+<img
+	src={url}
+	alt="badge"
+	class="{$$props.class} h-[1rem] w-[1rem] bg-center bg-contain bg-no-repeat"
+/>
