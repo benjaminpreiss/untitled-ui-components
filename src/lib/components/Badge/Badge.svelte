c59863e40c0524b868b8e28aa65b18f6d317aab2
Nadiem <nadiem8@msn.com>
2023-10-10 12:23:44 +0200

    Update BadgeWrapper and Badge add clickable function and correct padding and margin

--- src/lib/components/Badge/Badge.svelte
+++ src/lib/components/Badge/Badge.svelte
@@ -4,66 +4,37 @@
 	import type { size, icon, color } from './types.js';
 	import IconAtom from './IconAtom.svelte';
 	import DotAtom from './DotAtom.svelte';
-	import xclose from '$lib/assets/icons/general/x-close.svg?url';
-	import dummyCountry from '$lib/assets/icons/dummies/au.svg?url';
-	import dummyAvatar from '$lib/assets/icons/dummies/avatar.svg?url';
-	import { setContext } from 'svelte';
+	// TODO can remove the raw if we just go for the svg from figma
 	import ImageAtom from './ImageAtom.svelte';
 	import type { HTMLButtonAttributes } from 'svelte/elements';
+	import XcloseAtom from './XcloseAtom.svelte';
 
-	// todo fix the onclick as props later
-
-	/* TODO: specify props type
-	
 	interface $$Props extends HTMLButtonAttributes {
 		size: size;
 		color: color;
-		icon: icon;
+		icon?: icon;
 		outline: boolean;
+		onClick?: (() => void) | undefined;
 	}
-
-	Hint: Whenever you need a property from $$props, just cast it: 
-
-	const onClick = ($$props as $$Props)["on:click"]
-	
-	*/
-
-	/* 
-	TODO: for x-close without leading specified, import svg as raw and adjust stroke width
-	import xClose from '$lib/assets/icons/general/x-close.svg?raw';
-
-	<div>{@html xClose}</div>
-	Hint: set stroke width via tailwind class or via style tag
-	*/
-	/* TODO: for all other ICONS, use svg as mask and only adjust the color, NOT THE STROKE */
-	/* TODO: for all image */
 
 	export let size: size;
 	export let color: color;
 	export let icon: icon | undefined = undefined;
 	export let outline: boolean = false;
-	// to do how can we pass in on:click, but access the $$props['on:click'] property to be able to set the badge to a button or not
-	$: console.log($$props);
-	// currently added a clickable boolean
-	export let clickable: boolean = false;
-
-	setContext('icon', icon);
+	export let onClick: (() => void) | undefined = undefined;
 </script>
 
-<BadgeWrapper {clickable} {outline} {...$$props} {size} {color} on:click>
+<BadgeWrapper {outline} {...$$props} {size} {color}>
 	{#if icon}
 		{#if icon.type === 'dot'}
 			<DotAtom slot="icon-left" />
 			<TextAtom><slot /></TextAtom>
-		{:else if icon.type === 'avatar'}
-			<ImageAtom slot="icon-left" url={icon.leading ?? dummyAvatar} />
-			<TextAtom><slot /></TextAtom>
-		{:else if icon.type === 'country'}
-			<ImageAtom slot="icon-left" url={icon.leading ?? dummyCountry} />
+		{:else if icon.type === 'image'}
+			<ImageAtom slot="icon-left" url={icon.leading} />
 			<TextAtom><slot /></TextAtom>
 		{:else if icon.type === 'x-close'}
 			<TextAtom><slot /></TextAtom>
-			<IconAtom slot="icon-right" url={icon.trailing ?? xclose} />
+			<XcloseAtom onClick={icon.onClick} slot="icon-right" url={icon.trailing || undefined} />
 		{:else if icon.type === 'icon-only'}
 			<IconAtom url={icon.leading} />
 		{:else if icon.type === 'icon'}
--- src/lib/components/Badge/BadgeWrapper.svelte
+++ src/lib/components/Badge/BadgeWrapper.svelte
@@ -1,169 +1,129 @@
 <script lang="ts">
 	import type { HTMLButtonAttributes } from 'svelte/elements';
-	import type { size, color, icon } from './types.js';
+	import type { size, color } from './types.js';
 	import { getContext, setContext } from 'svelte';
 	interface $$Props extends HTMLButtonAttributes {
 		size: size;
 		outline: boolean;
 		color?: color;
-		clickable?: boolean;
+		onClick?: (() => void) | undefined;
 	}
 	export let size: size;
 	export let outline: boolean = false;
 	export let color: color = 'primary';
+	export let onClick: (() => void) | undefined = undefined;
 
-	export let clickable: boolean = false;
-
-	// TODO: Remove icon here
-	const icon: icon = getContext('icon');
 	setContext('color', color);
 	setContext('outline', outline);
 	setContext('size', size);
 
-	// TODO: Remove trailing
-	const trailing = !!(icon && ((icon.type === 'icon' && icon.trailing) || icon.type === 'x-close'));
-
 	const styleColorsButtons = {
 		outline: {
 			gray: {
-				coloring: 'text-untld-gray-600  border-untld-gray-700'
+				coloring: 'text-untld-gray-600  ring-untld-gray-700'
 			},
 			primary: {
-				coloring: ' text-untld-primary-600  border-untld-primary-700   '
+				coloring: ' text-untld-primary-600  ring-untld-primary-700   '
 			},
 			error: {
-				coloring: 'text-untld-error-600  border-untld-error-700   '
+				coloring: 'text-untld-error-600  ring-untld-error-700   '
 			},
 			warning: {
-				coloring: ' text-untld-warning-600  border-untld-warning-700   '
+				coloring: ' text-untld-warning-600  ring-untld-warning-700   '
 			},
 			success: {
-				coloring: ' text-untld-success-600  border-untld-success-700   '
+				coloring: ' text-untld-success-600  ring-untld-success-700   '
 			},
 			'gray-blue': {
-				coloring: ' text-untld-gray-blue-600  border-untld-gray-blue-700      '
+				coloring: ' text-untld-gray-blue-600  ring-untld-gray-blue-700      '
 			},
 			'blue-light': {
-				coloring: ' text-untld-blue-light-600  border-untld-blue-light-700    '
+				coloring: ' text-untld-blue-light-600  ring-untld-blue-light-700    '
 			},
 			blue: {
-				coloring: ' text-untld-blue-600 border-untld-blue-700    '
+				coloring: ' text-untld-blue-600 ring-untld-blue-700    '
 			},
 			indigo: {
-				coloring: ' text-untld-indigo-600 border-untld-indigo-700    '
+				coloring: ' text-untld-indigo-600 ring-untld-indigo-700    '
 			},
 			purple: {
-				coloring: ' text-untld-purple-600 border-untld-purple-700    '
+				coloring: ' text-untld-purple-600 ring-untld-purple-700    '
 			},
 			pink: {
-				coloring: ' text-untld-pink-600 border-untld-pink-700    '
+				coloring: ' text-untld-pink-600 ring-untld-pink-700    '
 			},
 			rosé: {
-				coloring: ' text-untld-rosé-600 border-untld-rosé-700    '
+				coloring: ' text-untld-rosé-600 ring-untld-rosé-700    '
 			},
 			orange: {
-				coloring: ' text-untld-orange-600 border-untld-orange-700    '
+				coloring: ' text-untld-orange-600 ring-untld-orange-700    '
 			}
 		},
 		noOutline: {
 			gray: {
-				coloring: 'bg-untld-gray-100 text-untld-gray-700 border-untld-gray-100 '
+				coloring: 'bg-untld-gray-100 text-untld-gray-700 ring-untld-gray-100 '
 			},
 			primary: {
-				coloring: 'bg-untld-primary-50 text-untld-primary-700 border-untld-primary-50     '
+				coloring: 'bg-untld-primary-50 text-untld-primary-700 ring-untld-primary-50     '
 			},
 			error: {
-				coloring: 'bg-untld-error-50 text-untld-error-700 border-untld-error-50    '
+				coloring: 'bg-untld-error-50 text-untld-error-700 ring-untld-error-50    '
 			},
 			warning: {
-				coloring: 'bg-untld-warning-50 text-untld-warning-700 border-untld-warning-50 '
+				coloring: 'bg-untld-warning-50 text-untld-warning-700 ring-untld-warning-50 '
 			},
 			success: {
-				coloring: 'bg-untld-success-50 text-untld-success-700  border-untld-success-50   '
+				coloring: 'bg-untld-success-50 text-untld-success-700  ring-untld-success-50   '
 			},
 			'gray-blue': {
-				coloring: 'bg-untld-gray-blue-50 text-untld-gray-blue-600  border-untld-gray-blue-50      '
+				coloring: 'bg-untld-gray-blue-50 text-untld-gray-blue-600  ring-untld-gray-blue-50      '
 			},
 			'blue-light': {
-				coloring: 'bg-untld-blue-light-50 text-untld-blue-light-600  border-untld-blue-light-50    '
+				coloring: 'bg-untld-blue-light-50 text-untld-blue-light-600  ring-untld-blue-light-50    '
 			},
 			blue: {
-				coloring: 'bg-untld-blue-50 text-untld-blue-700 border-untld-blue-50   '
+				coloring: 'bg-untld-blue-50 text-untld-blue-700 ring-untld-blue-50   '
 			},
 			indigo: {
-				coloring: 'bg-untld-indigo-50 text-untld-indigo-700 border-untld-indigo-50  '
+				coloring: 'bg-untld-indigo-50 text-untld-indigo-700 ring-untld-indigo-50  '
 			},
 			purple: {
-				coloring: 'bg-untld-purple-50 text-untld-purple-700 border-untld-purple-50   '
+				coloring: 'bg-untld-purple-50 text-untld-purple-700 ring-untld-purple-50   '
 			},
 			pink: {
-				coloring: 'bg-untld-pink-50 text-untld-pink-700 border-untld-pink-50  '
+				coloring: 'bg-untld-pink-50 text-untld-pink-700 ring-untld-pink-50  '
 			},
 			rosé: {
-				coloring: 'bg-untld-rosé-50 text-untld-rosé-700 border-untld-rosé-50 my-0    '
+				coloring: 'bg-untld-rosé-50 text-untld-rosé-700 ring-untld-rosé-50 my-0    '
 			},
 			orange: {
-				coloring: 'bg-untld-orange-50 text-untld-orange-700 border-untld-orange-50   '
+				coloring: 'bg-untld-orange-50 text-untld-orange-700 ring-untld-orange-50   '
 			}
 		}
 	};
 
-	// TODO: Unify paddings here so that they don't depend on icon
-
 	const stylePaddings = {
 		sm: {
-			padding: `${
-				icon && (icon.type === 'avatar' || icon.type === 'country')
-					? 'gap-1.5 pl-[0.094rem] pr-[0.406rem] py-[0.031rem] '
-					: icon && icon.type === 'icon-only'
-					? 'p-[0.156rem] gap-0'
-					: icon && icon.type === 'dot'
-					? 'pl-[0.281rem] py-[0.031rem] pr-[0.406rem] gap-1'
-					: trailing
-					? 'pr-[0.281rem] py-[0.031rem] pl-[0.406rem] gap-1'
-					: 'pl-[0.281rem] py-[0.031rem] pr-[0.406rem] gap-1'
-			} `
+			padding: 'px-[0.5rem] py-[0.125rem] gap-1.5 '
 		},
 		md: {
-			padding: ` ${
-				icon && (icon.type === 'avatar' || icon.type === 'country')
-					? ' gap-1.5 pl-[0.156rem] pr-[0.531rem] py-[0.031rem] '
-					: icon && icon.type === 'icon-only'
-					? 'p-[0.281rem] gap-0'
-					: icon && icon.type === 'dot'
-					? 'pl-[0.406rem] py-[0.031rem] pr-[0.531rem] gap-1.5'
-					: trailing
-					? 'pr-[0.406rem] py-[0.031rem] pl-[0.531rem] gap-1'
-					: 'pl-[0.406rem] py-[0.031rem] pr-[0.531rem] gap-1'
-			} `
+			padding: ' px-[0.625rem] py-[0.125rem] gap-2'
 		},
 		lg: {
-			padding: `${
-				icon && (icon.type === 'avatar' || icon.type === 'country')
-					? ' gap-1.5 pl-[0.281rem] pr-[0.656rem] py-[0.156rem] '
-					: icon && icon.type === 'icon-only'
-					? 'p-[0.406rem] gap-0'
-					: icon && icon.type === 'dot'
-					? 'pl-[0.531rem] py-[0.156rem] pr-[0.656rem] gap-1.5'
-					: trailing
-					? 'pr-[0.531rem] py-[0.156rem] pl-[0.656rem] gap-1'
-					: 'pl-[0.531rem] py-[0.156rem] pr-[0.656rem] gap-1'
-			} `
+			padding: 'px-[0.75rem] py-[0.25rem] gap-2'
 		}
 	};
 </script>
 
-<!-- TODO Replace border with an inset ring! -->
-<!-- TODO check if the onClick hasOwnProperty works -->
 <button
 	{...$$props}
+	on:click={onClick}
 	class="{$$props.class}  
     {styleColorsButtons[outline ? 'outline' : 'noOutline'][color].coloring}
     {stylePaddings[size].padding}
-    {clickable
+    {onClick
 		? 'pointer-events-auto'
-		: 'pointer-events-none'} flex justify-center items-center outline-none border-[0.094rem] rounded-full disabled:pointer-events-none focus-visible:outline-none focus:outline-none"
-	on:click
+		: 'pointer-events-auto cursor-default'} flex justify-center items-center outline-none ring-[0.09375rem] rounded-full disabled:pointer-events-none focus-visible:outline-none focus:outline-none"
 >
 	<slot name="icon-left" />
 	<slot />
