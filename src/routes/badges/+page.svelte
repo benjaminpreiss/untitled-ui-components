c570c51afde16eb9067b9b7c364df3c2e4110de8
Nadiem <nadiem8@msn.com>
2023-10-10 12:23:58 +0200

    Update badges page and simplify logic

--- src/routes/badges/+page.svelte
+++ src/routes/badges/+page.svelte
@@ -1,15 +1,5 @@
 <script lang="ts">
 	import Badge from '$lib/components/Badge/Badge.svelte';
-	import dummyCountry from '$lib/assets/icons/dummies/au.svg?url';
-	import dummyAvatar from '$lib/assets/icons/dummies/avatar.svg?url';
-	import arrowRight from '$lib/assets/icons/arrows/arrow-right.svg?url';
-	import arrowUp from '$lib/assets/icons/arrows/arrow-up.svg?url';
-	import plus from '$lib/assets/icons/general/plus.svg?url';
-
-	import BadgeIconAtom from '$lib/components/Badge/IconAtom.svelte';
-	console.log(xClose);
-
-	import type { color, icon, size } from '$lib/components/Badge/types.js';
 	import { browser } from '$app/environment';
 
 	type color =
@@ -44,280 +34,235 @@
 		'orange'
 	];
 	const sizes: size[] = ['sm', 'md', 'lg'];
-
-	const noIconNoOutline: { color: color; outline: boolean; size: size }[] = colors.flatMap((c) =>
-		sizes.map((s) => ({ color: c, outline: false, size: s }))
-	);
-
-	const noIconOutline: { color: color; outline: boolean; size: size }[] = colors.flatMap((c) =>
-		sizes.map((s) => ({ color: c, outline: true, size: s }))
-	);
-
-	const dotNoOutline: { color: color; outline: boolean; size: size; icon: icon }[] = colors.flatMap(
-		(c) => sizes.map((s) => ({ color: c, outline: false, size: s, icon: { type: 'dot' } }))
-	);
-
-	const dotOutline: { color: color; outline: boolean; size: size; icon: icon }[] = colors.flatMap(
-		(c) => sizes.map((s) => ({ color: c, outline: true, size: s, icon: { type: 'dot' } }))
-	);
-
-	const avatarNoOutline: { color: color; outline: boolean; size: size; icon: icon }[] =
-		colors.flatMap((c) =>
-			sizes.map((s) => ({
-				color: c,
-				outline: false,
-				size: s,
-				icon: { type: 'avatar', leading: dummyAvatar }
-			}))
-		);
-
-	const avatarOutline: { color: color; outline: boolean; size: size; icon: icon }[] =
-		colors.flatMap((c) =>
-			sizes.map((s) => ({
-				color: c,
-				outline: true,
-				size: s,
-				icon: { type: 'avatar', leading: dummyAvatar }
-			}))
-		);
-	const countryNoOutline: { color: color; outline: boolean; size: size; icon: icon }[] =
-		colors.flatMap((c) =>
-			sizes.map((s) => ({
-				color: c,
-				outline: false,
-				size: s,
-				icon: { type: 'country', leading: dummyCountry }
-			}))
-		);
-	const countryOutline: { color: color; outline: boolean; size: size; icon: icon }[] =
-		colors.flatMap((c) =>
-			sizes.map((s) => ({
-				color: c,
-				outline: true,
-				size: s,
-				icon: { type: 'country', leading: dummyCountry }
-			}))
-		);
-	const xCloseNoOutline: { color: color; outline: boolean; size: size; icon: icon }[] =
-		colors.flatMap((c) =>
-			sizes.map((s) => ({
-				color: c,
-				outline: false,
-				size: s,
-				icon: { type: 'x-close' }
-			}))
-		);
-	const xCloseOutline: { color: color; outline: boolean; size: size; icon: icon }[] =
-		colors.flatMap((c) =>
-			sizes.map((s) => ({
-				color: c,
-				outline: true,
-				size: s,
-				icon: { type: 'x-close' }
-			}))
-		);
-	const iconLeadingNoOutline: { color: color; outline: boolean; size: size; icon: icon }[] =
-		colors.flatMap((c) =>
-			sizes.map((s) => ({
-				color: c,
-				outline: false,
-				size: s,
-				icon: { type: 'icon', leading: arrowUp }
-			}))
-		);
-	const iconLeadingOutline: { color: color; outline: boolean; size: size; icon: icon }[] =
-		colors.flatMap((c) =>
-			sizes.map((s) => ({
-				color: c,
-				outline: true,
-				size: s,
-				icon: { type: 'icon', leading: arrowUp }
-			}))
-		);
-	const iconTrailingNoOutline: { color: color; outline: boolean; size: size; icon: icon }[] =
-		colors.flatMap((c) =>
-			sizes.map((s) => ({
-				color: c,
-				outline: false,
-				size: s,
-				icon: { type: 'icon', trailing: arrowRight }
-			}))
-		);
-	const iconTrailingOutline: { color: color; outline: boolean; size: size; icon: icon }[] =
-		colors.flatMap((c) =>
-			sizes.map((s) => ({
-				color: c,
-				outline: true,
-				size: s,
-				icon: { type: 'icon', trailing: arrowRight }
-			}))
-		);
-
-	const noIconNoTextNoOutline: { color: color; outline: boolean; size: size; icon: icon }[] =
-		colors.flatMap((c) =>
-			sizes.map((s) => ({
-				color: c,
-				outline: false,
-				size: s,
-				icon: { type: 'icon-only', leading: plus }
-			}))
-		);
-
-	const noIconNoTextOutline: { color: color; outline: boolean; size: size; icon: icon }[] =
-		colors.flatMap((c) =>
-			sizes.map((s) => ({
-				color: c,
-				outline: true,
-				size: s,
-				icon: { type: 'icon-only', leading: plus }
-			}))
-		);
 </script>
+
+<div />
 
 {#if browser}
 	<div class=" flex w-full items-center justify-center p-12 overflow-x-auto min-w-max gap-8">
 		<div class="flex flex-col space-y-8">
 			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[14.5rem]">
-				{#each noIconNoOutline as { color, outline, size }}
-					<Badge {size} {color} {outline}>Label</Badge>
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge
+							{size}
+							{color}
+							outline={false}
+							onClick={() =>
+								console.log(
+									`Clicked on the Label with size: ` +
+										size +
+										` color: ` +
+										color +
+										` and outline:` +
+										false
+								)}>Label</Badge
+						>
+					{/each}
 				{/each}
 			</div>
-
 			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[14.5rem]">
-				{#each noIconOutline as { color, outline, size }}
-					<Badge {size} {color} {outline}>Label</Badge>
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge
+							{size}
+							{color}
+							outline={true}
+							onClick={() =>
+								console.log(
+									`Clicked on the Label with size: ` +
+										size +
+										`  color: ` +
+										color +
+										` and outline:` +
+										true
+								)}>Label</Badge
+						>
+					{/each}
+				{/each}
+			</div>
+		</div>
+
+		<div class="flex flex-col space-y-8">
+			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[14.5rem]">
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge {size} {color} outline={false} icon={{ type: 'dot' }}>Label</Badge>
+					{/each}
+				{/each}
+			</div>
+			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[14.5rem]">
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge {size} {color} outline={true} icon={{ type: 'dot' }}>Label</Badge>
+					{/each}
+				{/each}
+			</div>
+		</div>
+
+		<div class="flex flex-col space-y-8">
+			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge
+							{size}
+							{color}
+							outline={false}
+							icon={{ type: 'image', leading: '/dummies/au.svg' }}>Label</Badge
+						>
+					{/each}
+				{/each}
+			</div>
+			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge
+							{size}
+							{color}
+							outline={true}
+							icon={{ type: 'image', leading: '/dummies/au.svg' }}>Label</Badge
+						>
+					{/each}
+				{/each}
+			</div>
+		</div>
+
+		<div class="flex flex-col space-y-8">
+			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge
+							{size}
+							{color}
+							outline={false}
+							icon={{ type: 'image', leading: '/dummies/avatar.svg' }}>Label</Badge
+						>
+					{/each}
+				{/each}
+			</div>
+			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge
+							{size}
+							{color}
+							outline={true}
+							icon={{ type: 'image', leading: '/dummies/avatar.svg' }}>Label</Badge
+						>
+					{/each}
 				{/each}
 			</div>
 		</div>
 		<div class="flex flex-col space-y-8">
 			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
-				{#each dotNoOutline as { color, outline, size, icon }}
-					<Badge {icon} {size} {color} {outline}>Label</Badge>
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge
+							{size}
+							{color}
+							outline={false}
+							icon={{ type: 'x-close', onClick: () => console.log('click click') }}>Label</Badge
+						>
+					{/each}
 				{/each}
 			</div>
-
 			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
-				{#each dotOutline as { color, outline, size, icon }}
-					<Badge {icon} {size} {color} {outline}>Label</Badge>
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge
+							{size}
+							{color}
+							outline={true}
+							icon={{ type: 'x-close', onClick: () => console.log('click click') }}>Label</Badge
+						>
+					{/each}
 				{/each}
 			</div>
 		</div>
 		<div class="flex flex-col space-y-8">
 			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
-				{#each countryNoOutline as { color, outline, size, icon }}
-					<Badge {icon} {size} {color} {outline}>Label</Badge>
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge
+							{size}
+							{color}
+							outline={false}
+							icon={{ type: 'icon', trailing: 'icons/arrow-right.svg' }}>Label</Badge
+						>
+					{/each}
 				{/each}
 			</div>
-
 			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
-				{#each countryOutline as { color, outline, size, icon }}
-					<Badge {icon} {size} {color} {outline}>Label</Badge>
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge
+							{size}
+							{color}
+							outline={true}
+							icon={{ type: 'icon', trailing: 'icons/arrow-right.svg' }}>Label</Badge
+						>
+					{/each}
 				{/each}
 			</div>
 		</div>
 		<div class="flex flex-col space-y-8">
 			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
-				{#each avatarNoOutline as { color, outline, size, icon }}
-					<Badge {icon} {size} {color} {outline}>Label</Badge>
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge
+							{size}
+							{color}
+							outline={false}
+							icon={{ type: 'icon', trailing: 'icons/arrow-up.svg' }}>Label</Badge
+						>
+					{/each}
 				{/each}
 			</div>
-
 			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
-				{#each avatarOutline as { color, outline, size, icon }}
-					<Badge {icon} {size} {color} {outline}>Label</Badge>
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge
+							{size}
+							{color}
+							outline={true}
+							icon={{ type: 'icon', trailing: 'icons/arrow-up.svg' }}>Label</Badge
+						>
+					{/each}
 				{/each}
 			</div>
 		</div>
-		<div class="flex flex-col space-y-8">
-			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
-				{#each xCloseNoOutline as { color, outline, size, icon }}
-					<Badge
-						clickable={true}
-						{icon}
-						{size}
-						{color}
-						{outline}
-						on:click={() => console.log('this badge is clickable')}>Label</Badge
-					>
-				{/each}
-			</div>
 
-			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
-				{#each xCloseOutline as { color, outline, size, icon }}
-					<Badge
-						clickable={true}
-						{icon}
-						{size}
-						{color}
-						{outline}
-						on:click={() => console.log('this badge is clickable')}>Label</Badge
-					>
-				{/each}
-			</div>
-		</div>
 		<div class="flex flex-col space-y-8">
-			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
-				{#each iconLeadingNoOutline as { color, outline, size, icon }}
-					<Badge {icon} {size} {color} {outline}>Label</Badge>
+			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[6rem]">
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge
+							onClick={() => console.log('button clicked')}
+							{size}
+							{color}
+							outline={false}
+							icon={{
+								type: 'icon-only',
+								leading: 'icons/plus.svg'
+							}}
+						/>
+					{/each}
 				{/each}
 			</div>
-
-			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
-				{#each iconLeadingOutline as { color, outline, size, icon }}
-					<Badge {icon} {size} {color} {outline}>Label</Badge>
-				{/each}
-			</div>
-		</div>
-		<div class="flex flex-col space-y-8">
-			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
-				{#each iconTrailingNoOutline as { color, outline, size, icon }}
-					<Badge {icon} {size} {color} {outline}>Label</Badge>
-				{/each}
-			</div>
-
-			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
-				{#each iconTrailingOutline as { color, outline, size, icon }}
-					<Badge {icon} {size} {color} {outline}>Label</Badge>
-				{/each}
-			</div>
-		</div>
-		<div class="flex flex-col space-y-8">
-			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
-				{#each iconTrailingNoOutline as { color, outline, size, icon }}
-					<Badge {icon} {size} {color} {outline}>Label</Badge>
-				{/each}
-			</div>
-
-			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[15.5rem]">
-				{#each iconTrailingOutline as { color, outline, size, icon }}
-					<Badge {icon} {size} {color} {outline}>Label</Badge>
-				{/each}
-			</div>
-		</div>
-		<div class="flex flex-col space-y-8">
-			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[6.5rem]">
-				{#each noIconNoTextNoOutline as { color, outline, size, icon }}
-					<Badge
-						clickable={true}
-						{icon}
-						{size}
-						{color}
-						{outline}
-						on:click={() => console.log('this badge is clickable')}
-					/>
-				{/each}
-			</div>
-
-			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[6.5rem]">
-				{#each noIconNoTextOutline as { color, outline, size, icon }}
-					<Badge
-						clickable={true}
-						{icon}
-						{size}
-						{color}
-						{outline}
-						on:click={() => console.log('this badge is clickable')}
-					/>
+			<div class="flex flex-wrap col-span-3 justify-center items-center gap-2 w-[6rem]">
+				{#each colors as color}
+					{#each sizes as size}
+						<Badge
+							onClick={() => console.log('button clicked')}
+							{size}
+							{color}
+							outline={true}
+							icon={{
+								type: 'icon-only',
+								leading: 'icons/plus.svg'
+							}}>Label</Badge
+						>
+					{/each}
 				{/each}
 			</div>
 		</div>
